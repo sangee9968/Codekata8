42f2a41a43c88ead035a8a611bf9ca9414586eb9
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-01-29 19:35:54 +0530

    Update composite_or_not.py

--- composite_or_not.py
+++ composite_or_not.py
@@ -1,13 +1,10 @@
 n=int(input())
-if n==2:
-   print("no")
-for i in range(2,n):
-   if n%i==0:
-      flag=0
-      break
-   else: 
-      flag=1
-if flag==0:
-   print("no")
+factor=0
+for i in range(1,n):
+  if n%i==0:
+    factor=i
+if factor>1:
+  print("yes")
 else:
-   print("yes")
+  #print result
+  print("no")
